0eb96233451598511548eef2134312959697940f
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-01-11 19:24:50 +0700

    create your own module

--- app.js
+++ app.js
@@ -3,12 +3,22 @@
 // fs.copyFileSync("text.txt", "text2.txt")
 // console.log('text.txt was copied to text2.txt');
 
-const hero = require("superheroes")
-const nameHero = hero.random()
+// Module NPM
+// const hero = require("superheroes")
+// const nameHero = hero.random()
 
-// console.log(nameHero)
+// // console.log(nameHero)
 
-for (let index = 0; index < 10; index++) {
-    // console.log(nameHero)
-    console.log(hero.random())
-}
+// for (let index = 0; index < 10; index++) {
+//     // console.log(nameHero)
+//     console.log(hero.random())
+// }
+
+const op = require("./module")
+
+const moduleTambah = op.tambah(10, 5)
+const moduleKali = op.kali(10, 5)
+
+console.log(op.title)
+console.log(moduleTambah)
+console.log(moduleKali)
