59e167060e6ef04741e0b724f295aa1b8c4e3bca
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-01-11 19:15:47 +0700

    module npm

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fnd-29-node-js
+
+Reference from [Node JavaScript Dasar](https://www.buildwithangga.com/kelas/node-javascript-dasar?thumbnail=wATNuz4FZX.36&main_leads=searchresult)
--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const fs = require("fs")
+// const fs = require("fs")
 
-fs.copyFileSync("text.txt", "text2.txt")
-console.log('text.txt was copied to text2.txt');
+// fs.copyFileSync("text.txt", "text2.txt")
+// console.log('text.txt was copied to text2.txt');
+
+const hero = require("superheroes")
+const nameHero = hero.random()
+
+// console.log(nameHero)
+
+for (let index = 0; index < 10; index++) {
+    // console.log(nameHero)
+    console.log(hero.random())
+}
